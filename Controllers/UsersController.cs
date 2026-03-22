8ac4f55cd342abd88f2f16fe9fc9853be29e61d4
highlander1337 <tassioleno@gmail.com>
2026-03-22 00:47:19 -0300

    Remove comment

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -28,9 +28,6 @@ public class UsersController : ControllerBase
 
     // GET api/users/{id}
     [HttpGet("{id:guid}")]
-
-    // how to use swagger to show the possible responses for this endpoint? for example, if the user is not found, it should return a 404 with a message. If the user is found, it should return a 200 with the user details. How can I document this in swagger so that it's clear to API consumers? 
-    // To document the possible responses for this endpoint in Swagger, you can use the [ProducesResponseType] attribute to specify the different HTTP status codes and their corresponding response types. In this case, you can indicate that a 200 OK response will return a user detail object, while a 404 Not Found response will return an error message. Here's how you can do it: 
     [ProducesResponseType(typeof(UserDetailResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetById(Guid id)
